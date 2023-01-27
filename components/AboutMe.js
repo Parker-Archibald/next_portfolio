8a7e1a93ee7ca264cd7e6f688ace237e138ed7fe
Parker-Archibald <parkerarchibald97@gmail.com>
2023-01-27 12:32:17 -0700

    aboutme

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -4,11 +4,11 @@ import aboutMePic from '../styles/Pictures/AboutMePic.jpg';
 const AboutMe = () => {
 
     return (
-        <div className="h-screen">
-            <div className="flex flex-col text-center">
-                <h1 className="text-2xl font-bold ">About me</h1>
-                <div className="flex">
-                    <div><Image src={aboutMePic} className='w-3/12 rounded-2xl '/></div>
+        <div className="">
+            <div className="h-screen flex flex-col text-center">
+                <h1 className="text-2xl font-bold">About me</h1>
+                <div className="h-full flex items-center">
+                    <div className=""><Image src={aboutMePic} className='sm:w-1/2 lg:w-2/5 rounded-2xl m-4'/></div>
                     <div>text</div>
                 </div>
             </div>
